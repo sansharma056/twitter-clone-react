13782663c6745b8b8f2ba1a093b659c9150944dc
Sankalp Sharma <sansharma056@gmail.com>
2020-11-19 12:29:02 +0530

    Added useInput custom hook, form handler

--- src/MiniLogin.js
+++ src/MiniLogin.js
@@ -1,15 +1,36 @@
 import React from "react";
 import Input from "./Input";
-import { Link } from "@reach/router";
+import useInput from "./useInput";
 
 const MiniLogin = () => {
+  const email = useInput("");
+  const password = useInput("");
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    if (!email.state || !password.state) {
+      throw new Error("Empty Fields");
+    }
+    console.log(email.state, password.state);
+  };
+
   return (
-    <form className="mini-login">
-      <Input type="email" labelName="Email" />
-      <Input type="password" labelName="Password" />
-      <Link className="btn" to="/">
+    <form className="mini-login" onSubmit={handleForm}>
+      <Input
+        type="email"
+        labelName="Email"
+        value={email.value}
+        onChange={email.onChange}
+      />
+      <Input
+        type="password"
+        labelName="Password"
+        value={password.value}
+        onChange={password.onChange}
+      />
+      <button type="submit" className="btn" to="/">
         Log in
-      </Link>
+      </button>
     </form>
   );
 };
