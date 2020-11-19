20d6180ef5473c6ff2f638d784efc50a4e019362
Sankalp Sharma <sansharma056@gmail.com>
2020-11-20 01:26:03 +0530

    New Component made up of Main and Footer

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Main from "./Main";
+import Footer from "./Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Main />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
