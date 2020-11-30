e1636393f19797f1bdc8c6a07524983ee104fa4a
Sankalp Sharma <sansharma056@gmail.com>
2020-11-30 18:12:05 +0530

    Added route for MainScreen

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Router } from "@reach/router";
 import Home from "./Home";
 import Login from "./Login";
 import Signup from "./Signup";
+import MainScreen from "./MainScreen";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Home path="/" />
       <Login path="login" />
       <Signup path="signup" />
+      <MainScreen path="/*" />
     </Router>
   );
 };
