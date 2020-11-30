372650f2a5b0690fbcc3362df6be7cf9b0f63b22
Sankalp Sharma <sansharma056@gmail.com>
2020-11-30 18:05:30 +0530

    Avatar  shows default image if no URL is passed

--- src/Avatar.js
+++ src/Avatar.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import defaultImage from "./default_profile_75_75.png";
 const Avatar = ({ src }) => {
   return (
     <div className="avatar">
-      <img src={src} alt="Avatar" />
+      <img src={src ? src : defaultImage} alt="Avatar" />
     </div>
   );
 };
