ceeb5b0a16fa64188a9cb60286a9b9f484daeb87
Sankalp Sharma <sansharma056@gmail.com>
2020-12-03 04:48:24 +0530

    Added support for different dimensions of profile picture

--- src/Avatar.js
+++ src/Avatar.js
@@ -1,9 +1,23 @@
 import React from "react";
 import defaultImage from "./default_profile_75_75.png";
-const Avatar = ({ src }) => {
+import defaultImageMedium from "./default_profile_128_128.png";
+import defaultImageLarge from "./default_profile_400_400.png";
+
+const Avatar = ({ src, size }) => {
+  const defaultPicSrc = function (size) {
+    switch (size) {
+      case "large":
+        return defaultImageLarge;
+      case "medium":
+        return defaultImageMedium;
+      default:
+        return defaultImage;
+    }
+  };
+
   return (
     <div className="avatar">
-      <img src={src ? src : defaultImage} alt="Avatar" />
+      <img src={src ? src : defaultPicSrc(size)} alt="Avatar" />
     </div>
   );
 };
