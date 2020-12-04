1c8b142752bbbc75b24b580c10456213c2d0ea5e
Sankalp Sharma <sansharma056@gmail.com>
2020-12-05 04:07:21 +0530

    Added value prop to SearchInput

--- src/SearchInput.js
+++ src/SearchInput.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { SearchIcon } from "./Icons";
 
-const SearchInput = ({ onChange }) => {
+const SearchInput = ({ onChange, value }) => {
   return (
     <div className="search-input">
       <SearchIcon />
       <input
         type="text"
         placeholder="Seach Twitter Clone"
+        value={value}
         onChange={onChange}
       />
     </div>
