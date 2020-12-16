69933e2ddd0e88c95a6c5450ea9468e43875cb80
Sankalp Sharma <sansharma056@gmail.com>
2020-12-17 05:26:55 +0530

    Connected Login to API and added error message

--- src/Login.js
+++ src/Login.js
@@ -1,24 +1,46 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { TwitterLogo } from "./Icons";
 import Input from "./Input";
 import useInput from "./useInput";
 import Modal from "./Modal";
 import useModal from "./useModal";
 import Signup from "./Signup";
+import { AuthContext } from "./AuthContext";
+import { useNavigate } from "@reach/router";
+import axios from "axios";
 
 const Login = () => {
   const { isModalVisible, toggleModal } = useModal(false);
-
   const email = useInput("");
   const password = useInput("");
+  const [isErrorMessageVisible, setIsErrorMessageVisible] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const authState = useContext(AuthContext);
+  const navigate = useNavigate();
 
   const handleForm = (e) => {
     e.preventDefault();
+
     if (!email.state || !password.state) {
       throw new Error("Empty Fields");
     }
-    console.log(email.state, password.state);
-    document.getElementsByTagName("button").disabled = "true";
+
+    axios({
+      method: "POST",
+      url: "http://localhost:3000/api/signin",
+      data: { email: email.state, password: password.state },
+    })
+      .then((response) => {
+        authState.signin();
+        console.log(`Bearer ${response.data.token}`);
+        navigate("/home");
+      })
+      .catch((error) => {
+        setErrorMessage(error.response.data.message);
+        setIsErrorMessageVisible(true);
+        email.setState("");
+        password.setState("");
+      });
   };
 
   useEffect(() => {
@@ -30,6 +52,11 @@ const Login = () => {
       <form className="login-form" onSubmit={handleForm} noValidate>
         <TwitterLogo />
         <h1>Log in to Twitter Clone</h1>
+        {isErrorMessageVisible ? (
+          <div className="error-wrapper">
+            <span className="error">{errorMessage}</span>
+          </div>
+        ) : null}
         <div className="input-wrapper">
           <Input
             type="email"
