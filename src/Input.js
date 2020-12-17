e515c343bce82fc6c1dd810fa01f83830804f075
Sankalp Sharma <sansharma056@gmail.com>
2020-12-18 00:29:27 +0530

    Added isInvalid property to Input for styling

--- src/Input.js
+++ src/Input.js
@@ -1,8 +1,11 @@
 import React from "react";
 
-const Input = ({ labelName, type, id, value, onChange }) => {
+const Input = ({ labelName, type, id, value, onChange, isInvalid }) => {
   return (
-    <div className="input-group">
+    <div
+      className={`input-group ${isInvalid ? "input-group-invalid" : null}`}
+      id={`input-group-${id}`}
+    >
       <label htmlFor={id}>{labelName}</label>
       <input type={type} id={id} value={value} onChange={onChange} />
     </div>
