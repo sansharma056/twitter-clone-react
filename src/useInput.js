430421fe2b9f4ed1ed407a0db27be4926c82d55e
Sankalp Sharma <sansharma056@gmail.com>
2020-12-18 00:21:43 +0530

    Added isTouched property which basically means that input is visited at least once

--- src/useInput.js
+++ src/useInput.js
@@ -2,12 +2,14 @@ import { useState } from "react";
 
 const useInput = (defaultState) => {
   const [state, setState] = useState(defaultState);
+  const [isTouched, setIsTouched] = useState(false);
 
   const onChange = (e) => {
     setState(e.target.value);
+    setIsTouched(true);
   };
 
-  return { state, setState, onChange };
+  return { state, setState, onChange, isTouched };
 };
 
 export default useInput;
