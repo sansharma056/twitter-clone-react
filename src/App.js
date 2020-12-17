aa31db010e3bc473f6253c1f55f4bb0306c99eef
Sankalp Sharma <sansharma056@gmail.com>
2020-12-17 05:34:13 +0530

    Added AuthContext provider to App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { Router } from "@reach/router";
 
 import Home from "./Home";
 import Login from "./Login";
-import Signup from "./Signup";
 import MainScreen from "./MainScreen";
+import { AuthContext } from "./AuthContext";
 
 const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const signin = () => {
+    setIsAuthenticated(true);
+  };
+
+  const signup = () => {
+    setIsAuthenticated(false);
+  };
+
   return (
-    <Router>
-      <Home path="/" />
-      <Login path="login" />
-      <Signup path="signup" />
-      <MainScreen path="/*" />
-    </Router>
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        signin,
+        signup,
+      }}
+    >
+      <Router>
+        <Home path="/" />
+        <Login path="login" />
+        <MainScreen path="/*" />
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
