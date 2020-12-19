28c7fe7de46d937f4bd9619a9de89ab7a76c02a1
Sankalp Sharma <sansharma056@gmail.com>
2020-12-20 04:07:33 +0530

    Store screenName in AuthContext

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -3,6 +3,7 @@ import { createContext } from "react";
 export const AuthContext = createContext({
   isAuthenticated: false,
   token: null,
+  screenName: null,
   signin: () => {},
   signout: () => {},
 });
