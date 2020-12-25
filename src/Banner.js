1cf093c73ffed6d0722d59072198fbd7f48da2ee
Sankalp Sharma <sansharma056@gmail.com>
2020-12-25 22:29:34 +0530

    Wrapped Banner in a div

--- src/Banner.js
+++ src/Banner.js
@@ -2,8 +2,10 @@ import React from "react";
 import defaultBanner from "./default_banner_600_200.png";
 const Banner = ({ src }) => {
   return (
-    <div className="banner">
-      <img src={src ? src : defaultBanner} alt="Banner" />
+    <div className="banner-wrapper">
+      <div className="banner">
+        <img src={src ? src : defaultBanner} alt="" />
+      </div>
     </div>
   );
 };
