6f32692cd02f4151283da2820170bab259a5379a
Sankalp Sharma <sansharma056@gmail.com>
2021-03-15 22:03:43 +0530

    Replaced placeholder data with actual API call in Bookmarks

--- src/Bookmarks.js
+++ src/Bookmarks.js
@@ -1,31 +1,57 @@
-import React from "react";
+import React, { useContext, useState, useMemo, useCallback } from "react";
+import { AuthContext } from "./AuthContext";
+import Spinner from "./Spinner";
 import Tweet from "./Tweet";
+import useAxiosFetch from "./useAxiosFetch";
 
 const Bookmarks = () => {
-  const bookmarks = [
-    {
-      id: 0,
-      avatarURL: "",
-      name: "John Doe",
-      handle: "johndoe",
-      content: "foo1\nfoo2\nfoo3\nfoo4",
-    },
-  ];
-  const user = {
-    handle: "johndoe",
-  };
-  // Replace with API call
-  return (
+  const [state, setState] = useState({ bookmarks: [], loading: true });
+  const authState = useContext(AuthContext);
+
+  const options = useMemo(
+    () => ({
+      method: "GET",
+      url: `${process.env.API_URL}/user/${authState.screenName}`,
+      headers: { authorization: authState.token },
+    }),
+    [authState.token, authState.screenName]
+  );
+
+  const onFetch = useCallback(function onFetch(response) {
+    if (response.status === 200) {
+      setState({ bookmarks: response.data.user.bookmarks, loading: false });
+    }
+  }, []);
+
+  const onError = useCallback(function onError(error) {
+    console.log(error);
+  }, []);
+
+  const onCancel = useCallback(function onCancel(error) {
+    console.log(error);
+  }, []);
+
+  useAxiosFetch(options, {
+    onFetch,
+    onError,
+    onCancel,
+  });
+
+  return state.loading ? (
+    <Spinner />
+  ) : (
     <div className="bookmarks">
       <div className="bookmarks-header">
         <div className="bookmarks-header-content">
           <h2>Bookmarks</h2>
-          <p>{`@${user.handle}`}</p>
+          <p>{`@${authState.screenName}`}</p>
         </div>
       </div>
       <div className="bookmarks-content">
-        {bookmarks ? (
-          bookmarks.map((tweet) => <Tweet key={tweet.id} tweetData={tweet} />)
+        {state.bookmarks.length ? (
+          state.bookmarks.map((tweet) => (
+            <Tweet key={tweet} id={tweet} tweetData={tweet} />
+          ))
         ) : (
           <div className="bookmarks-content-wrapper">
             <h2>You haven’t added any Tweets to your Bookmarks yet</h2>
