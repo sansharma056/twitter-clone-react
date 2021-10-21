94485038a972fd53bb08c6a453b62831e9a34939
Sankalp Sharma <sansharma056@gmail.com>
2021-10-22 01:24:40 +0530

    redirecting non logged in users to Login

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Router, Link, useLocation } from "@reach/router";
+import React, { useContext } from "react";
+import { Router, Link, useLocation, Redirect } from "@reach/router";
 import UserHome from "./UserHome";
 import Nav from "./Nav";
 import WhoToFollow from "./WhoToFollow";
@@ -9,49 +9,54 @@ import Profile from "./Profile";
 import Bookmarks from "./Bookmarks";
 import SearchInput from "./SearchInput";
 import useInput from "./useInput";
+import { AuthContext } from "./AuthContext";
 
 const MainScreen = () => {
-  const searchInput = useInput("");
-  const { pathname } = useLocation();
+	const authState = useContext(AuthContext);
+	const searchInput = useInput("");
+	const { pathname } = useLocation();
 
-  return (
-    <div className="main-screen">
-      <div className="sidebar">
-        <div className="sidebar-header-wrapper">
-          <div className="sidebar-header">
-            <Link className="btn btn--icon" to="home">
-              <TwitterLogo />
-            </Link>
-            <Nav />
-          </div>
-        </div>
-      </div>
-      <div className="main-screen-content">
-        <div>
-          <Router className="main-screen-router">
-            <UserHome path="home" />
-            <Explore path="explore" />
-            <Explore path="explore/:q" />
-            <Bookmarks path="bookmarks" />
-            <Profile path=":screenName" />
-          </Router>
-          <div className="sidebar-right">
-            {pathname != "/explore" ? (
-              <SearchInput
-                onChange={searchInput.onChange}
-                value={searchInput.state}
-              />
-            ) : null}
-            <WhoToFollow
-              users={[
-                { id: 1, avatarURL: "", name: "Test Test", handle: "test" },
-                { id: 2, avatarURL: "", name: "Test1 Test", handle: "test1" },
-              ]}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return authState.isAuthenticated ? (
+		<div className="main-screen">
+			<div className="sidebar">
+				<div className="sidebar-header-wrapper">
+					<div className="sidebar-header">
+						<Link className="btn btn--icon" to="home">
+							<TwitterLogo />
+						</Link>
+						<Nav />
+					</div>
+				</div>
+			</div>
+			<div className="main-screen-content">
+				<div>
+					<Router className="main-screen-router">
+						<UserHome path="home" />
+						<Explore path="explore" />
+						<Explore path="explore/:q" />
+						<Bookmarks path="bookmarks" />
+						<Profile path=":screenName" />
+					</Router>
+					<div className="sidebar-right">
+						{pathname != "/explore" ? (
+							<SearchInput
+								onChange={searchInput.onChange}
+								value={searchInput.state}
+							/>
+						) : null}
+						<WhoToFollow
+							users={[
+								{ id: 1, avatarURL: "", name: "Test Test", handle: "test" },
+								{ id: 2, avatarURL: "", name: "Test1 Test", handle: "test1" },
+							]}
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	) : (
+		<Redirect to="login" noThrow />
+	);
 };
+
 export default MainScreen;
