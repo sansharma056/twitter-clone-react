c68ff528927d927a82cc9f9357b0c502bba4c7f9
Sankalp Sharma <sansharma056@gmail.com>
2021-10-22 23:07:24 +0530

    persist authentication state

--- src/App.js
+++ src/App.js
@@ -13,48 +13,52 @@ import jwt_decode from "jwt-decode";
 dotenv.config();
 
 const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [token, setToken] = useState(localStorage.getItem("token"));
-  const [screenName, setScreenName] = useState(
-    localStorage.getItem("screenName")
-  );
+	const [isAuthenticated, setIsAuthenticated] = useState(
+		localStorage.getItem("isAuthenticated") ?? false
+	);
+	const [token, setToken] = useState(localStorage.getItem("token"));
+	const [screenName, setScreenName] = useState(
+		localStorage.getItem("screenName")
+	);
 
-  const signin = (token) => {
-    const payload = jwt_decode(token);
+	const signin = (token) => {
+		const payload = jwt_decode(token);
 
-    setIsAuthenticated(true);
-    setToken(`Bearer ${token}`);
-    setScreenName(payload.screenName);
+		setIsAuthenticated(true);
+		setToken(`Bearer ${token}`);
+		setScreenName(payload.screenName);
 
-    localStorage.setItem("token", `Bearer ${token}`);
-    localStorage.setItem("screenName", payload.screenName);
-  };
+		localStorage.setItem("token", `Bearer ${token}`);
+		localStorage.setItem("screenName", payload.screenName);
+		localStorage.setItem("isAuthenticated", true);
+	};
 
-  const signout = () => {
-    setIsAuthenticated(false);
-    setToken(null);
-    setScreenName(null);
-    localStorage.removeItem("token");
-    localStorage.removeItem("screenName");
-  };
+	const signout = () => {
+		setIsAuthenticated(false);
+		setToken(null);
+		setScreenName(null);
+		localStorage.removeItem("token");
+		localStorage.removeItem("screenName");
+		localStorage.removeItem("isAuthenticated");
+	};
 
-  return (
-    <AuthContext.Provider
-      value={{
-        isAuthenticated,
-        token,
-        screenName,
-        signin,
-        signout,
-      }}
-    >
-      <Router>
-        <Home path="/" />
-        <Login path="login/*" />
-        <MainScreen path="/*" />
-      </Router>
-    </AuthContext.Provider>
-  );
+	return (
+		<AuthContext.Provider
+			value={{
+				isAuthenticated,
+				token,
+				screenName,
+				signin,
+				signout,
+			}}
+		>
+			<Router>
+				<Home path="/" />
+				<Login path="login/*" />
+				<MainScreen path="/*" />
+			</Router>
+		</AuthContext.Provider>
+	);
 };
 
 render(<App />, document.getElementById("react-root"));
