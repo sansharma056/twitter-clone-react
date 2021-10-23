55c5ea50605602cac05a5de7eab88edfe67cabac
Sankalp Sharma <sansharma056@gmail.com>
2021-10-23 06:17:59 +0530

    migrated to parcel 2

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Link } from "@reach/router";
 import MiniLogin from "./MiniLogin";
 import { CommentIcon, SearchIcon, TwitterLogo, UserFriendsIcon } from "./Icons";
